98dcddc7b4f7a4bd9c6e18b4df3312f06c53396f
Andrew Dobbs <andrew.dobbs@gmail.com>
2020-06-06 18:30:35 -0400

    Add camera following

--- uia-chapter-6/Assets/Scripts/PlatformPlayer.cs
+++ uia-chapter-6/Assets/Scripts/PlatformPlayer.cs
@@ -28,18 +28,30 @@ public class PlatformPlayer : MonoBehaviour {
 		Vector2 corner2 = new Vector2(min.x, min.y - .2f);
 		Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
 		bool grounded = false;
+		MovingPlatform platform = null;
+		Vector3 pScale = Vector3.one;
 
 		if (hit != null) {
 			grounded = true;
+			platform = hit.GetComponent<MovingPlatform>();
 		}
 
+		if (platform != null) {
+			transform.parent = platform.transform;
+			pScale = platform.transform.localScale;
+		} else {
+			transform.parent = null;
+		}
+
+		if (deltaX != 0) {
+			transform.localScale = new Vector3(Mathf.Sign(deltaX) / pScale.x, 1 / pScale.y, 1);
+		}
+
+		_body.gravityScale = grounded && deltaX == 0 ? 0 : 1;
 		if (grounded && Input.GetKeyDown(KeyCode.Space)) {
 			_body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
 		}
 		
 		_anim.SetFloat("speed", Mathf.Abs(deltaX));
-		if (!Mathf.Approximately(deltaX, 0)) {
-			transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1);
-		}
 	}
 }
