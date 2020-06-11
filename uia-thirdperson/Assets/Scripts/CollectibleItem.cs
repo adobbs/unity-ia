9f0b681069e8502428fb5b98c3b0e2e8ace19d6c
Andrew Dobbs <andrew.dobbs@gmail.com>
2020-06-11 14:43:06 -0400

    Add triggers to doors and items

---
+++ uia-thirdperson/Assets/Scripts/CollectibleItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleItem : MonoBehaviour {
+
+	[SerializeField] private string itemName;
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log("Item collected: " + itemName);
+		Destroy(this.gameObject);
+	}
+}
