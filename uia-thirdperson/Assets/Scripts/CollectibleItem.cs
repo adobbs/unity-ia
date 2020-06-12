ae7add4476b94b1240f35e282d800e3bcfe04d26
Andrew Dobbs <andrew.dobbs@gmail.com>
2020-06-12 19:48:01 -0400

    Add inventory with consumable and equipable items

--- uia-thirdperson/Assets/Scripts/CollectibleItem.cs
+++ uia-thirdperson/Assets/Scripts/CollectibleItem.cs
@@ -7,7 +7,7 @@ public class CollectibleItem : MonoBehaviour {
 	[SerializeField] private string itemName;
 
 	void OnTriggerEnter(Collider other) {
-		Debug.Log("Item collected: " + itemName);
+		Managers.Inventory.AddItem(itemName);
 		Destroy(this.gameObject);
 	}
 }
